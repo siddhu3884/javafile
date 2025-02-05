9293997c555e43263425c5080f0c9dbf4d69c1e7
siddhu3884 <siddhugundabolu2@gmail.com>
2025-02-05 15:24:22 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,9 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        // Print a simple greeting message
+        System.out.println("Hello, World!");
+    }
+}
+
+    
+
---
+++ VariablesAndDatatypes.java
@@ -0,0 +1,21 @@
+public class VariablesAndDatatypes{
+    
+     public static void main(String[] args) {
+        // Declare and initialize different data types
+        int integerVar = 13;
+        double doubleVar = 23.5;
+        char charVar = 'A';
+        String stringVar = "Hello, This is pravallika";
+        boolean boolVar = true;
+
+        // Display the values of the variables
+        System.out.println("Integer: " + integerVar);
+        System.out.println("Double: " + doubleVar);
+        System.out.println("Character: " + charVar);
+        System.out.println("String: " + stringVar);
+        System.out.println("Boolean: " + boolVar);
+    }
+}
+
+    
+
