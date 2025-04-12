4d542d2022a515c0085f3f4c32cf9d8676390363
siddhu3884 <siddhugundabolu2@gmail.com>
2025-04-12 10:44:56 +0530

    Add files via upload

---
+++ boxpattern.java
@@ -0,0 +1,17 @@
+
+public class boxpattern
+{
+    public static void main(String[] args)
+    {
+        int n=5,i,j;
+        for(i=0;i<=n;i++)
+        {
+            for(j=0;j<=5;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
