0dd8f8a7ea37dca811f7c7171c9ec5d1c9554eb0
siddhu3884 <siddhugundabolu2@gmail.com>
2025-04-12 10:39:15 +0530

    Add files via upload

---
+++ SimpleArrayListExample.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class SimpleArrayListExample {
+    public static void main(String[] args) {
+        // Create an ArrayList
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        // Add some numbers
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+
+        // Print the list
+        System.out.println("Numbers: " + numbers);
+    }
+}
